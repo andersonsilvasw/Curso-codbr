bf5936dd74705b7cec1351481d5e45f925fe5ea7
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-11-28 10:58:23 -0300

    Interfaces Funcionais - Aulas 3 e 4

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -1,7 +1,16 @@
 package lambdas;
 
+@FunctionalInterface // Essa annotation obriga que a interface tenha apenas um método abstrato.
 public interface Calculo {
 	
 	double executar(double a, double b);
+	
+	default String legal() {
+		return "Legal";
+	}
+	
+	static String muitoLegal() {
+		return "Muito Legal";
+	}
 
 }
